8b4570913a5ff6b4e026f0d04c981a35fa22763e
jdelacruz23 <924460027@sfsu.edu>
2025-05-14 02:07:38 -0700

    made frontend better, made the gemini response to be just plain text

---
+++ scripts_/gemini_api_call.js
@@ -0,0 +1,24 @@
+// For Node.js environments
+import 'dotenv/config'
+import { GoogleGenAI } from '@google/genai';
+
+//API SECRET
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
+//Create a Gemini Client
+const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+
+// main func declaration
+export async function gemini_api_call(user_query) {
+    const response = await ai.models.generateContent({
+        model: 'gemini-2.0-flash-001',
+        contents: user_query,
+    });
+   
+    // const responseJson = JSON.stringify(response, null, 2);
+    // return responseJson
+    const text = response.candidates?.[0]?.content?.parts?.[0]?.text || 'No response received.';
+    return text;
+}
+
+// main();
